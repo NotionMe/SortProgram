8ee554454625b1fa2b8cdb8a0817ce3d3e0f4cd8
NotionMe <c.ubohyi.stanislav@student.uzhnu.edu.ua>
2025-04-18 16:50:23 +0300

    Update

--- obj/Debug/net9.0/Practika2_OPAM_Ubohyi_Stanislav.AssemblyInfo.cs
+++ obj/Debug/net9.0/Practika2_OPAM_Ubohyi_Stanislav.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Practika2_OPAM_Ubohyi_Stanislav")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4623858af7722082ed30e1b9c40700643739f53")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56cbbbfe673aea8666bb328afba6d13b98098046")]
 [assembly: System.Reflection.AssemblyProductAttribute("Practika2_OPAM_Ubohyi_Stanislav")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Practika2_OPAM_Ubohyi_Stanislav")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
