6ed520a54f1324ed30b4a2f8c58218fa0c776313
NotionMe <c.ubohyi.stanislav@student.uzhnu.edu.ua>
2025-04-30 18:45:52 +0300

    Update Information

--- Pages/InfoAlgoritm/Algoritm.axaml.cs
+++ Pages/InfoAlgoritm/Algoritm.axaml.cs
@@ -1,14 +1,71 @@
 using System;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.LogicalTree;
 using Avalonia.Markup.Xaml;
+using Avalonia.VisualTree;
 
-namespace Practika2_OPAM_Ubohyi_Stanislav.Pages.InfoAlgoritm
+namespace Practika2_OPAM_Ubohyi_Stanislav.Pages.InfoAlgoritm;
+
+public partial class Algoritm : UserControl
 {
-    public partial class Algoritm : UserControl
+    public Algoritm()
     {
-        public Algoritm()
+        InitializeComponent();
+    }
+
+    private void BackToHome_Click(object? sender, RoutedEventArgs e)
+    {
+        var mainWindow = this.GetVisualRoot() as SortProgram;
+        if (mainWindow != null)
         {
-            InitializeComponent();
+            mainWindow.NavigateToPagePublic(new HomePage());
+            return;
         }
+        
+        var mainContainer = FindParent<ContentControl>(this);
+        if (mainContainer != null)
+        {
+            mainContainer.Content = new HomePage();
+        }
+    }
+    private void InfoDateStructureButton_Click(object? sender, RoutedEventArgs e)
+    {
+        var dataStructurePage = new DataStructure();
+        NavigatePage(dataStructurePage);
+    }
+
+    private void NavigatePage(UserControl page)
+    {
+        var mainWindow = this.GetVisualRoot() as SortProgram;
+        if (mainWindow != null)
+        {
+            mainWindow.NavigateToPagePublic(page);
+            return;
+        }
+        
+        var mainContainer = FindParent<ContentControl>(this);
+        if (mainContainer != null)
+        {
+            mainContainer.Content = page;
+        }
+    }
+    
+    private T? FindParent<T>(Control control) where T : class
+    {
+        // Check visual hierarchy
+        var current = control;
+        while (current != null)
+        {
+            if (current is T result)
+                return result;
+            
+            if (current.Parent is T parentResult)
+                return parentResult;
+                
+            current = current.Parent as Control;
+        }
+        
+        return null;
     }
 }
--- Pages/InfoAlgoritm/DataStructure.axaml.cs
+++ Pages/InfoAlgoritm/DataStructure.axaml.cs
@@ -1,7 +1,9 @@
 using System;
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using Avalonia.VisualTree;
 
 namespace Practika2_OPAM_Ubohyi_Stanislav.Pages.InfoAlgoritm;
 
@@ -10,5 +12,60 @@ public partial class DataStructure : UserControl
     public DataStructure()
     {
         InitializeComponent();
+    }
+    private void InfoAlgoritm_Clock(object? sender, RoutedEventArgs e)
+    {
+        var algoritmPage = new Algoritm();
+        NavigatePage(algoritmPage);
+    }
+
+    private void BackToHome_Click(object? sender, RoutedEventArgs e)
+    {
+        var mainWindow = this.GetVisualRoot() as SortProgram;
+        if (mainWindow != null)
+        {
+            mainWindow.NavigateToPagePublic(new HomePage());
+            return;
+        }
+        
+        var mainContainer = FindParent<ContentControl>(this);
+        if (mainContainer != null)
+        {
+            mainContainer.Content = new HomePage();
+        }
+    }
+
+    private void NavigatePage(UserControl page)
+    {
+        var mainWindow = this.GetVisualRoot() as SortProgram;
+        if (mainWindow != null)
+        {
+            mainWindow.NavigateToPagePublic(page);
+            return;
+        }
+        
+        var mainContainer = FindParent<ContentControl>(this);
+        if (mainContainer != null)
+        {
+            mainContainer.Content = page;
+        }
+    }
+    
+    private T? FindParent<T>(Control control) where T : class
+    {
+        // Check visual hierarchy
+        var current = control;
+        while (current != null)
+        {
+            if (current is T result)
+                return result;
+            
+            if (current.Parent is T parentResult)
+                return parentResult;
+                
+            current = current.Parent as Control;
+        }
+        
+        return null;
     }
 }
